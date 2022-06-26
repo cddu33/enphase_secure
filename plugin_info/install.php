5628a3580b6b30163c7ad12708a7851b40e9aeb7
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-06-26 18:00:16 +0200

    dev

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -28,7 +28,7 @@ function fordcar_install() {
 function fordcar_update() {
 	fordcar::dependancy_install();
   exec('../ressources/install_apt.sh');
-	fordcar::refresh();
+	fordcarCmd::refresh();
 }
 
 // Fonction exécutée automatiquement après la suppression du plugin
