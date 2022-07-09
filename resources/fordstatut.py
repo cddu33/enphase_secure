a8c4eba809ffd405e7082579b03e2459c3c65ba7
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 15:38:45 +0200

    gg

--- resources/fordstatut.py
+++ resources/fordstatut.py
@@ -4,12 +4,12 @@ import sys, os, logging, time, json
 from fordpass import Vehicle
 
 if len(sys.argv) != 6:
-	print('Il y a ', len(sys.argv), ' arguments au lieu de 5: user: ', sys.argv[1], ' / pass: ', sys.argv[2], ' / vin: ', sys.argv[3], ' / commande: ',sys.argv[4], ' / adresse: ', sys.argv[5])
+	print('Il y a un problème avec les arguments: ', sys.argv[1], ' / pass: ', sys.argv[2], ' / vin: ', sys.argv[3], ' / commande: ',sys.argv[4], ' / adresse: ', sys.argv[5])
 else:
 	r = Vehicle(sys.argv[1], sys.argv[2], sys.argv[3]) # Username, Password, VIN # Username, Password, VIN
+	print(r.status())
 	try:
 		with open(sys.argv[5], "w+") as json_file:
-			print('ok')
 			st = r.status()
 			print(st)
 			json.dump(st, json_file)
