662c4cc3b9b1474b9c016272c7afb8823d0c355d
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 15:32:18 +0200

    r

--- resources/fordstatut.py
+++ resources/fordstatut.py
@@ -4,7 +4,7 @@ import sys, os, logging, time, json
 from fordpass import Vehicle
 
 if len(sys.argv) != 5:
-	print('Il y a '+len(sys.argv)+'au lieu de 5: user / pass / vin / commande / adresse')
+	print('Il y a ', len(sys.argv), 'au lieu de 5: user / pass / vin / commande / adresse')
 else:
 	r = Vehicle(sys.argv[1], sys.argv[2], sys.argv[3]) # Username, Password, VIN # Username, Password, VIN
 	try:
