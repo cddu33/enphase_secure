35194a347d04414de4519b69ae9863de73cad09a
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 15:29:16 +0200

    d

--- resources/fordstatut.py
+++ resources/fordstatut.py
@@ -4,7 +4,7 @@ import sys, os, logging, time, json
 from fordpass import Vehicle
 
 if len(sys.argv) != 5:
-	raise Exception('Must specify Username, Password and VIN as arguments, e.g. demo.py test@test.com password123 WX231231232')
+	print('Il y a '. len(sys.argv). 'au lieu de 5: user / pass / vin / commande / adresse')
 else:
 	r = Vehicle(sys.argv[1], sys.argv[2], sys.argv[3]) # Username, Password, VIN # Username, Password, VIN
 	try:
