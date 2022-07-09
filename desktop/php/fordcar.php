7874b1c10d22ca9924d64dbfc2262a5e1e13a362
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 19:42:57 +0200

    ok

--- desktop/php/fordcar.php
+++ desktop/php/fordcar.php
@@ -24,11 +24,13 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<br>
 				<span>{{Configuration}}</span>
 			</div>
+			<!--
 			<div class="cursor eqLogicAction logoSecondary" data-action="fordcar::refresh">
 				<i class="fas fa-wrench"></i>
 				<br>
 				<span>{{Forcer l'actualisation}}</span>
 			</div>
+			-->
 		</div>
 		<legend><i class="fas fa-table"></i> {{Mes véhicules}}</legend>
 		<?php
