696419d6a074756f068d020300fcb5395e8e5082
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 12:38:27 +0200

    55

--- core/class/fordcar.class.php
+++ core/class/fordcar.class.php
@@ -233,8 +233,8 @@ class fordcarCmd extends cmd {
   $fordcar_path = realpath(dirname(__FILE__));
 		foreach (self::byType('fordcar', true) as $fordcar) {
 			$cmd = 'python3 ' . $fordcar_path .'/../../resources/fordstatut.py';
-			//$cmd .= ' ' . $fordcar::byKey('user', 'fordcar') . ' ' . $fordcar::byKey('password', 'fordcar') . ' ' . $fordcar::byKey('vin', 'fordcar') .' ' . 'statut' . ' ' . '/../../data/'. $fordcar::byKey('vin', 'fordcar') . '.json' . ' ' . $fordcar_path;
-			$cmd .= ' ' . $fordcar->$getConfiguration("user") . ' ' . $fordcar->getConfiguration("password") . ' ' . $fordcar->getConfiguration("vin") .' ' . 'statut' . ' ' . '/../../data/'. $fordcar::byKey('vin', 'fordcar') . '.json' . ' ' . $fordcar_path;
+			$cmd .= ' ' . $fordcar::byKey('user', 'fordcar') . ' ' . $fordcar::byKey('password', 'fordcar') . ' ' . $fordcar::byKey('vin', 'fordcar') .' ' . 'statut' . ' ' . '/../../data/'. $fordcar::byKey('vin', 'fordcar') . '.json' . ' ' . $fordcar_path;
+			//$cmd .= ' ' . $fordcar->$getConfiguration("user") . ' ' . $fordcar->getConfiguration("password") . ' ' . $fordcar->getConfiguration("vin") .' ' . 'statut' . ' ' . '/../../data/'. $fordcar::byKey('vin', 'fordcar') . '.json' . ' ' . $fordcar_path;
 		
 			
 			log::add('fordcar', 'debug', 'commande ' . $cmd);
