c9f496faa615659ff0d1a7c06ae81fcee3da03cb
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 15:41:18 +0200

    ff

--- resources/fordstatut.py
+++ resources/fordstatut.py
@@ -4,7 +4,7 @@ import sys, os, logging, time, json
 from fordpass import Vehicle
 
 if len(sys.argv) != 6:
-	print('Il y a un problème avec les arguments: ', sys.argv[1], ' / pass: ', sys.argv[2], ' / vin: ', sys.argv[3], ' / commande: ',sys.argv[4], ' / adresse: ', sys.argv[5])
+	print('Il y a un probleme avec les arguments: ', sys.argv[1], ' / pass: ', sys.argv[2], ' / vin: ', sys.argv[3], ' / commande: ',sys.argv[4], ' / adresse: ', sys.argv[5])
 else:
 	r = Vehicle(sys.argv[1], sys.argv[2], sys.argv[3]) # Username, Password, VIN # Username, Password, VIN
 	print(r.status())
@@ -19,4 +19,3 @@ else:
 			r.unlock();
 	except:
 		print('Erreur de connexion')
-		print(sys.argv[5])
