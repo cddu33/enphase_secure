0d041c30366928c12c0ecd404a7bac09bf3b8366
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 17:37:57 +0200

    s

--- resources/fordstatut.py
+++ resources/fordstatut.py
@@ -7,10 +7,9 @@ if len(sys.argv) != 6:
 	print('Il y a un probleme avec les arguments: ', sys.argv[1], ' / pass: ', sys.argv[2], ' / vin: ', sys.argv[3], ' / commande: ',sys.argv[4], ' / adresse: ', sys.argv[5])
 else:
 	r = Vehicle(sys.argv[1], sys.argv[2], sys.argv[3]) # Username, Password, VIN # Username, Password, VIN
-	print(r.status())
 	try:
 		with open(sys.argv[5], "w+") as json_file:
-			print(r.status())
+			#print(r.status())
 			json.dump(r.status(), json_file)
 		if sys.argv[4] == "lock":
 			r.lock();
