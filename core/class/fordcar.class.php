c93457a8b861305aa1733c4daf6cc0c2942d0d15
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 12:47:01 +0200

    cc

--- core/class/fordcar.class.php
+++ core/class/fordcar.class.php
@@ -205,9 +205,6 @@ class fordcar extends eqLogic {
 		foreach (self::byType('fordcar', true) as $fordcar) {
 			$cmd = 'python3 ' . $fordcar_path .'/../../resources/fordstatut.py';
 			$cmd .= ' ' . $fordcar::byKey('user', 'fordcar') . ' ' . $fordcar::byKey('password', 'fordcar') . ' ' . $fordcar::byKey('vin', 'fordcar') .' ' . 'statut' . ' ' . '/../../data/'. $fordcar::byKey('vin', 'fordcar') . '.json' . ' ' . $fordcar_path;
-			//$cmd .= ' ' . $fordcar->$getConfiguration("user") . ' ' . $fordcar->getConfiguration("password") . ' ' . $fordcar->getConfiguration("vin") .' ' . 'statut' . ' ' . '/../../data/'. $fordcar::byKey('vin', 'fordcar') . '.json' . ' ' . $fordcar_path;
-		
-			
 			log::add('fordcar', 'debug', 'commande ' . $cmd);
 			//$cmd->execCmd();
 			exec($cmd . ' >> ' . log::getPathToLog('fordcar') . ' 2>&1 &');
