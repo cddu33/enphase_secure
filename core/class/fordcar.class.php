5d35d6452435809290eb59266d546d93ff4a875d
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 18:58:30 +0200

    clean

--- core/class/fordcar.class.php
+++ core/class/fordcar.class.php
@@ -22,11 +22,11 @@ class fordcar extends eqLogic {
   /*     * *************************Attributs****************************** */
 
 	public static function dependancy_info() {
-        $return = array();
+		$return = array();
         $return['log'] = log::getPathToLog(__CLASS__ . '_update');
         $return['progress_file'] = jeedom::getTmpFolder(__CLASS__) . '/dependency';
         if (file_exists(jeedom::getTmpFolder(__CLASS__) . '/dependency')) {
-            $return['state'] = 'in_progress';
+			$return['state'] = 'in_progress';
         } else {
             if (exec(system::getCmdSudo() . system::get('cmd_check') . '-Ec "python3\-requests"') < 1) { // adaptez la liste des paquets et le total
                 $return['state'] = 'nok';
@@ -38,12 +38,8 @@ class fordcar extends eqLogic {
     }
 
 	public static function dependancy_install() {
-		
-		//fordcar::refresh();
-		
 		log::remove(__CLASS__ . '_update');
 		return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh ' . jeedom::getTmpFolder('fordcar') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
-		
 	}
   
 
@@ -70,12 +66,12 @@ class fordcar extends eqLogic {
   * Fonction exécutée automatiquement toutes les minutes par Jeedom  */
  
   public static function cron() {
-	foreach (self::byType('fordcar', true) as $fordcar) { //parcours tous les équipements actifs du plugin vdm
-    $cmd = $fordcar->getCmd(null, 'refresh'); //retourne la commande "refresh" si elle existe
-    if (!is_object($cmd)) { //Si la commande n'existe pas
-    continue; //continue la boucle
-  }
-  $cmd->execCmd(); //la commande existe on la lance
+	  foreach (self::byType('fordcar', true) as $fordcar) { //parcours tous les équipements actifs du plugin vdm
+		  $cmd = $fordcar->getCmd(null, 'refresh'); //retourne la commande "refresh" si elle existe
+		  if (!is_object($cmd)) { //Si la commande n'existe pas
+			  continue; //continue la boucle
+			  }
+			  $cmd->execCmd(); //la commande existe on la lance
 }
 }
 
@@ -123,15 +119,14 @@ class fordcar extends eqLogic {
   // Fonction exécutée automatiquement avant la mise à jour de l'équipement
   public function preUpdate() {
 	 if ($this->getConfiguration('user') == '') {
-			throw new Exception('L\'identifiant ne peut pas être vide');
-	  }
-		if ($this->getConfiguration('password') == '') {
-			throw new Exception('Le mot de passe ne peut etre vide');
-		}
-		if ($this->getConfiguration('vin') == '') {
-			throw new Exception('Le VIN d\'identification du véhicule ne peut pas être vide');
-		}
-	  
+		 throw new Exception('L\'identifiant ne peut pas être vide');
+	 }
+	 if ($this->getConfiguration('password') == '') {
+		 throw new Exception('Le mot de passe ne peut etre vide');
+	 }
+	 if ($this->getConfiguration('vin') == '') {
+		 throw new Exception('Le VIN d\'identification du véhicule ne peut pas être vide');
+	 }
   }
 
   	
@@ -145,53 +140,50 @@ class fordcar extends eqLogic {
 
   // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
   public function postSave() {
-	   $refresh = $this->getCmd(null, 'refresh');
-  if (!is_object($refresh)) {
-    $refresh = new fordcarCmd();
-    $refresh->setName(__('Rafraichir', __FILE__));
-  }
- 
-  $refresh->setEqLogic_id($this->getId());
-  $refresh->setLogicalId('refresh');
-  $refresh->setType('action');
-  $refresh->setSubType('other');
-  $refresh->save();
+	  $refresh = $this->getCmd(null, 'refresh');
+	  if (!is_object($refresh)) {
+		  $refresh = new fordcarCmd();
+		  $refresh->setName(__('Rafraichir', __FILE__));
+	  }
+	  $refresh->setEqLogic_id($this->getId());
+	  $refresh->setLogicalId('refresh');
+	  $refresh->setType('action');
+	  $refresh->setSubType('other');
+	  $refresh->save();
   
-  $lock = $this->getCmd(null, 'lock');
-  if (!is_object($lock)) {
-    $lock = new fordcarCmd();
-    $lock->setName(__('Vérouiller', __FILE__));
+	  $lock = $this->getCmd(null, 'lock');
+	  if (!is_object($lock)) {
+		  $lock = new fordcarCmd();
+		  $lock->setName(__('Vérouiller', __FILE__));
+	  }
+	  $lock->setEqLogic_id($this->getId());
+	  $lock->setLogicalId('lock');
+	  $lock->setType('action');
+	  $lock->setSubType('other');
+	  $lock->save();
+	  
+	  $unlock = $this->getCmd(null, 'unlock');
+	  if (!is_object($unlock)) {
+		  $unlock = new fordcarCmd();
+		  $unlock->setName(__('Dévérouiller', __FILE__));
+	  }
+	  $unlock->setEqLogic_id($this->getId());
+	  $unlock->setLogicalId('unlock');
+	  $unlock->setType('action');
+	  $unlock->setSubType('other');
+	  $unlock->save();
+	  
+	  $etat = $this->getCmd(null, 'etat');
+	  if (!is_object($etat)) {
+		  $etat = new fordcarCmd();
+		  $etat->setName(__('Etat', __FILE__));
+	  }
+	  $etat->setEqLogic_id($this->getId());
+	  $etat->setLogicalId('etat');
+	  $etat->setType('info');
+	  $etat->setSubType('string');
+	  $etat->save();
   }
-  $lock->setEqLogic_id($this->getId());
-  $lock->setLogicalId('lock');
-  $lock->setType('action');
-  $lock->setSubType('other');
-  $lock->save();
-
-  $unlock = $this->getCmd(null, 'unlock');
-  if (!is_object($unlock)) {
-    $unlock = new fordcarCmd();
-    $unlock->setName(__('Dévérouiller', __FILE__));
-  }
-  $unlock->setEqLogic_id($this->getId());
-  $unlock->setLogicalId('unlock');
-  $unlock->setType('action');
-  $unlock->setSubType('other');
-  $unlock->save();
-
-  $etat = $this->getCmd(null, 'etat');
-  if (!is_object($etat)) {
-    $etat = new fordcarCmd();
-    $etat->setName(__('Etat', __FILE__));
-  }
-  $etat->setEqLogic_id($this->getId());
-  $etat->setLogicalId('etat');
-  $etat->setType('info');
-  $etat->setSubType('string');
-  $etat->save();
-  }
-
-
 
   // Fonction exécutée automatiquement avant la suppression de l'équipement
   public function preRemove() {
@@ -206,42 +198,36 @@ class fordcar extends eqLogic {
   * Exemple avec le champ "Mot de passe" (password)
    */
   public function decrypt() {
-    $this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
-	$this->setConfiguration('vin', utils::decrypt($this->getConfiguration('vin')));
-	
+	  $this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
+	  $this->setConfiguration('vin', utils::decrypt($this->getConfiguration('vin')));
   }
   public function encrypt() {
-    $this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
-	$this->setConfiguration('vin', utils::encrypt($this->getConfiguration('vin')));
+	  $this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
+	  $this->setConfiguration('vin', utils::encrypt($this->getConfiguration('vin')));
   }
  
 
   public function refresh() {
-
 	$fordcar_path = realpath(dirname(__FILE__));
-		foreach (self::byType('fordcar', true) as $fordcar) {
-			$fordcar_pass = $this->getConfiguration('password');
-			$fordcar_vin = $this->getConfiguration('vin');
-			$fordcar_user = $this->getConfiguration('user');
-			$fordcar_cmd = 'python3 ' . $fordcar_path .'/../../resources/fordstatut.py';
-			$fordcar_cmd .= ' ' . $fordcar_user . ' ' . $fordcar_pass . ' ' . $fordcar_vin .' ' . 'statut' . ' ' . $fordcar_path .'/../../data/'. $fordcar_vin . '.json';
-			log::add('fordcar', 'debug', 'commande ' . $fordcar_cmd);
-			//$cmd->execCmd();
-			exec($fordcar_cmd . ' >> ' . log::getPathToLog('fordcar') . ' 2>&1 &');
-		}
-  }
-  public function commandes($fordcar_statut) {
+	foreach (self::byType('fordcar', true) as $fordcar) {
 		$fordcar_pass = $this->getConfiguration('password');
 		$fordcar_vin = $this->getConfiguration('vin');
-			$fordcar_user = $this->getConfiguration('user');
-			$fordcar_cmd = 'python3 ' . realpath(dirname(__FILE__)) .'/../../resources/fordcmd.py';
-			$fordcar_cmd .= ' ' . $fordcar_user . ' ' . $fordcar_pass . ' ' . $fordcar_vin .' ' . $fordcar_statut ;
-			log::add('fordcar', 'debug', 'commande ' . $fordcar_cmd);
-			//$cmd->execCmd();
-			exec($fordcar_cmd . ' >> ' . log::getPathToLog('fordcar') . ' 2>&1 &');
-
+		$fordcar_user = $this->getConfiguration('user');
+		$fordcar_cmd = 'python3 ' . $fordcar_path .'/../../resources/fordstatut.py';
+		$fordcar_cmd .= ' ' . $fordcar_user . ' ' . $fordcar_pass . ' ' . $fordcar_vin .' ' . 'statut' . ' ' . $fordcar_path .'/../../data/'. $fordcar_vin . '.json';
+		log::add('fordcar', 'debug', 'commande ' . $fordcar_cmd);
+		exec($fordcar_cmd . ' >> ' . log::getPathToLog('fordcar') . ' 2>&1 &');
+	}
   }
-
+  public function commandes($fordcar_statut) {
+	  $fordcar_pass = $this->getConfiguration('password');
+	  $fordcar_vin = $this->getConfiguration('vin');
+	  $fordcar_user = $this->getConfiguration('user');
+	  $fordcar_cmd = 'python3 ' . realpath(dirname(__FILE__)) .'/../../resources/fordcmd.py';
+	  $fordcar_cmd .= ' ' . $fordcar_user . ' ' . $fordcar_pass . ' ' . $fordcar_vin .' ' . $fordcar_statut ;
+	  log::add('fordcar', 'debug', 'commande ' . $fordcar_cmd);
+	  exec($fordcar_cmd . ' >> ' . log::getPathToLog('fordcar') . ' 2>&1 &');
+  }
 }
 
 class fordcarCmd extends cmd {
@@ -253,23 +239,23 @@ class fordcarCmd extends cmd {
   public function dontRemoveCmd() {
     return true;
   }
-  
 
   // Exécution d'une commande
   public function execute($_options = array()) {
- $eqlogic = $this->getEqLogic(); //récupère l'éqlogic de la commande $this
-  switch ($this->getLogicalId()) { //vérifie le logicalid de la commande
-    case 'refresh': // LogicalId de la commande rafraîchir que l’on a créé dans la méthode Postsave de la classe vdm .
-    $info = $eqlogic->refresh(); //On lance la fonction randomVdm() pour récupérer une vdm et on la stocke dans la variable $info
-   break;
-   case 'lock': // LogicalId de la commande rafraîchir que l’on a créé dans la méthode Postsave de la classe vdm .
-    $info = $eqlogic->commandes("lock"); //On lance la fonction randomVdm() pour récupérer une vdm et on la stocke dans la variable $info*
-	break;
-	 case 'unlock': // LogicalId de la commande rafraîchir que l’on a créé dans la méthode Postsave de la classe vdm .
-    $info = $eqlogic->commandes("unlock"); //On lance la fonction randomVdm() pour récupérer une vdm et on la stocke dans la variable $info
-    break;
-  }
-  return $info;
+	  $eqlogic = $this->getEqLogic();
+	  switch ($this->getLogicalId()) 
+	  { 
+		  case 'refresh':
+		  $info = $eqlogic->refresh(); 
+		  break;
+		  case 'lock':
+		  $info = $eqlogic->commandes("lock"); 
+		  break;
+		  case 'unlock':
+		  $info = $eqlogic->commandes("unlock"); 
+		  break;
+	  }
+	  return $info;
   }
   /*     * **********************Getteur Setteur*************************** */
 
