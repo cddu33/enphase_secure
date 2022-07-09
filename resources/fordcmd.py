c615b79a478871e4c2b269ec5e3a6274caad1ad1
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-09 17:53:52 +0200

    n

---
+++ resources/fordcmd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys, os, logging, time, json
+
+from fordpass import Vehicle
+
+if len(sys.argv) != 6:
+	print('Il y a un probleme avec les arguments: ', sys.argv[1], ' / pass: ', sys.argv[2], ' / vin: ', sys.argv[3], ' / commande: ',sys.argv[4], ' / adresse: ', sys.argv[5])
+else:
+	r = Vehicle(sys.argv[1], sys.argv[2], sys.argv[3]) # Username, Password, VIN # Username, Password, VIN
+	try:
+		with open(sys.argv[5], "w+") as json_file:
+			#print(r.status())
+			json.dump(r.status(), json_file)
+		if sys.argv[4] == "lock":
+			r.lock();
+		elif sys.argv[4] == "unlock":
+			r.unlock();
+	except:
+		print('Erreur de connexion')
