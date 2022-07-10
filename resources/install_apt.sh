e079fa8b459321641292215d81adbab09b6a29ce
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-10 09:56:27 +0200

    correcetion pip

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -11,9 +11,11 @@ echo $(date)
 echo 5 > ${PROGRESS_FILE}
 sudo apt-get update
 echo 10 > ${PROGRESS_FILE}
-sudo apt-get install -y python3 python3-requests
+sudo apt-get install -y python3
 echo 20 > ${PROGRESS_FILE}
-sudo apt-get install pip
+sudo apt-get install -y python3-pip
+echo 30 > ${PROGRESS_FILE}
+sudo pip3 install requests
 echo 50 > ${PROGRESS_FILE}
 sudo python3 -m pip install git+https://github.com/NicKoehler/fordpass-python.git
 echo 100 > ${PROGRESS_FILE}
