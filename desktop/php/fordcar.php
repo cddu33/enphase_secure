d07ed73365e269a6788e2c371f421d210fa68519
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-08-03 20:34:13 +0200

    cron

--- desktop/php/fordcar.php
+++ desktop/php/fordcar.php
@@ -125,15 +125,21 @@ $eqLogics = eqLogic::byType($plugin->getId());
 							</div>
 
 				
-						<div class="form-group">
-						<label class="col-sm-2 control-label">{{Auto-actualisation (cron)}}</label>
-						<div class="col-sm-2">
-							<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Auto-actualisation (cron)}}"/>
-						</div>
-						<div class="col-sm-1">
-							<i class="fas fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
-						</div>
-					</div>
+							<div class="form-group">
+								<label class="col-sm-4 control-label">{{Auto-actualisation}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Fréquence de rafraîchissement des commandes infos du véhicule; On interoge le serveur pas directement la voiture}}"></i></sup>
+								</label>
+								<div class="col-sm-6">
+									<div class="input-group">
+										<input type="text" class="eqLogicAttr form-control roundedLeft" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Cliquer sur ? pour afficher l'assistant cron}}">
+										<span class="input-group-btn">
+											<a class="btn btn-default cursor jeeHelper roundedRight" data-helper="cron" title="Assistant cron">
+												<i class="fas fa-question-circle"></i>
+											</a>
+										</span>
+									</div>
+								</div>
+							</div>-->
 						</div>
                         
 						<div class="col-lg-6">
