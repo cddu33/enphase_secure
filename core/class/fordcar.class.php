c0880b15a19d47e7fc57173de7b2d75e6c3a12b1
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-08-18 18:00:19 +0200

    Update fordcar.class.php

--- core/class/fordcar.class.php
+++ core/class/fordcar.class.php
@@ -32,7 +32,7 @@ class fordcar extends eqLogic {
 		{
 			$return['state'] = 'nok';
 			$output = array();
-			$cmd = "pip3 list | grep fordpass";
+			$cmd = "sudo pyhton3 pip list | grep fordpass";
 			unset($output);
 			exec($cmd, $output, $return_var);
         
