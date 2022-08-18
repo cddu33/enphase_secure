2e5b99d0a36ac77e58df73109ba9bed13a46ac99
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-08-18 18:08:51 +0200

    Update install_apt_update.sh

--- resources/install_apt_update.sh
+++ resources/install_apt_update.sh
@@ -9,7 +9,7 @@ echo "*  Update des dépandances  *"
 echo "**********************************"
 echo $(date)
 echo 50 > ${PROGRESS_FILE}
-sudo python3 -m pip install --force git+https://github.com/cddu33/fordpass-python.git
+python3 -m pip install --force git+https://github.com/cddu33/fordpass-python.git
 echo 100 > ${PROGRESS_FILE}
 echo "**********************************"
 echo "*  Dépandances update OK  *"
