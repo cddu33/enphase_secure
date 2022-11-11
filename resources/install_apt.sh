73f37d96f3394ec31cb0f4120bed6f787d5cab83
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-11-11 11:35:01 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -18,7 +18,9 @@ sudo apt-get install -y python3-pip
 echo 30 > ${PROGRESS_FILE}
 sudo pip3 install --upgrade pip
 echo 40 > ${PROGRESS_FILE}
-sudo pip3 install pyjwt html.parser html5lib bs4 asyncio httpx lxml setuptools
+sudo apt-get install -y python3-setuptools
+echo 60 > ${PROGRESS_FILE}
+sudo pip3 install pyjwt html.parser html5lib bs4 asyncio httpx lxml
 rm ${PROGRESS_FILE}
 echo "**********************************"
 echo "*  Installation des dépendances OK  *"
